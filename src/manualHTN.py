058b5558d0b7ae3fcdc9ae9479a40ac8c931b262
jnguy405 <jnguy405@ucsc.edu>
2026-01-27 16:15:56 -0800

    Operators

--- src/manualHTN.py
+++ src/manualHTN.py
@@ -3,34 +3,235 @@ import pyhop
 '''begin operators'''
 
 def op_punch_for_wood(state, ID):
-	if state.time[ID] >= 4:
-		state.wood[ID] += 1
-		state.time[ID] -= 4
-		return state
-	return False
+    if state.time[ID] >= 4:
+        state.wood[ID] += 1
+        state.time[ID] -= 4
+        return state
+    return False
 
 def op_craft_wooden_axe_at_bench(state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.plank[ID] >= 3 and state.stick[ID] >=2:
-		state.wooden_axe[ID] += 1
-		state.plank[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.plank[ID] >= 3 and state.stick[ID] >= 2:
+        state.wooden_axe[ID] += 1
+        state.plank[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
 
-# Syntax for adding more operators:
-# op_def operator_name(state, ID):
-# if state.time[ID] >= required_time and state.resource1[ID] >= required_amount1 and ... :
-# state.resourceX[ID] += change_in_resourceX
-# state.resourceY[ID] -= change_in_resourceY
-# state.time[ID] -= required_time
-# return state
-# return False
-# ------------------------------------------------
+# Wood gathering with different axes
+def op_wooden_axe_for_wood(state, ID):
+    if state.time[ID] >= 2 and state.wooden_axe[ID] >= 1:
+        state.wood[ID] += 1
+        state.time[ID] -= 2
+        return state
+    return False
 
-# your code here
+def op_stone_axe_for_wood(state, ID):
+    if state.time[ID] >= 1 and state.stone_axe[ID] >= 1:
+        state.wood[ID] += 1
+        state.time[ID] -= 1
+        return state
+    return False
 
-pyhop.declare_operators(op_punch_for_wood, op_craft_wooden_axe_at_bench)
+def op_iron_axe_for_wood(state, ID):
+    if state.time[ID] >= 1 and state.iron_axe[ID] >= 1:
+        state.wood[ID] += 1
+        state.time[ID] -= 1
+        return state
+    return False
+
+# Basic crafting operations
+def op_craft_plank(state, ID):
+    if state.time[ID] >= 1 and state.wood[ID] >= 1:
+        state.plank[ID] += 4
+        state.wood[ID] -= 1
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_stick(state, ID):
+    if state.time[ID] >= 1 and state.plank[ID] >= 2:
+        state.stick[ID] += 4
+        state.plank[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_bench(state, ID):
+    if state.time[ID] >= 1 and state.plank[ID] >= 4:
+        state.bench[ID] += 1
+        state.plank[ID] -= 4
+        state.time[ID] -= 1
+        return state
+    return False
+
+# Tool crafting operations
+def op_craft_wooden_pickaxe_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.plank[ID] >= 3 and state.stick[ID] >= 2:
+        state.wooden_pickaxe[ID] += 1
+        state.plank[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_stone_axe_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 3 and state.stick[ID] >= 2:
+        state.stone_axe[ID] += 1
+        state.cobble[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_stone_pickaxe_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 3 and state.stick[ID] >= 2:
+        state.stone_pickaxe[ID] += 1
+        state.cobble[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_furnace_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.cobble[ID] >= 8:
+        state.furnace[ID] += 1
+        state.cobble[ID] -= 8
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_iron_axe_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 3 and state.stick[ID] >= 2:
+        state.iron_axe[ID] += 1
+        state.ingot[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_iron_pickaxe_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 3 and state.stick[ID] >= 2:
+        state.iron_pickaxe[ID] += 1
+        state.ingot[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
+
+# Smelting operation
+def op_smelt_ore_in_furnace(state, ID):
+    if state.time[ID] >= 5 and state.furnace[ID] >= 1 and state.coal[ID] >= 1 and state.ore[ID] >= 1:
+        state.ingot[ID] += 1
+        state.coal[ID] -= 1
+        state.ore[ID] -= 1
+        state.time[ID] -= 5
+        return state
+    return False
+
+# Gathering with pickaxes (coal, ore, cobble)
+def op_wooden_pickaxe_for_coal(state, ID):
+    if state.time[ID] >= 4 and state.wooden_pickaxe[ID] >= 1:
+        state.coal[ID] += 1
+        state.time[ID] -= 4
+        return state
+    return False
+
+def op_stone_pickaxe_for_coal(state, ID):
+    if state.time[ID] >= 2 and state.stone_pickaxe[ID] >= 1:
+        state.coal[ID] += 1
+        state.time[ID] -= 2
+        return state
+    return False
+
+def op_iron_pickaxe_for_coal(state, ID):
+    if state.time[ID] >= 1 and state.iron_pickaxe[ID] >= 1:
+        state.coal[ID] += 1
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_stone_pickaxe_for_ore(state, ID):
+    if state.time[ID] >= 4 and state.stone_pickaxe[ID] >= 1:
+        state.ore[ID] += 1
+        state.time[ID] -= 4
+        return state
+    return False
+
+def op_iron_pickaxe_for_ore(state, ID):
+    if state.time[ID] >= 2 and state.iron_pickaxe[ID] >= 1:
+        state.ore[ID] += 1
+        state.time[ID] -= 2
+        return state
+    return False
+
+def op_wooden_pickaxe_for_cobble(state, ID):
+    if state.time[ID] >= 4 and state.wooden_pickaxe[ID] >= 1:
+        state.cobble[ID] += 1
+        state.time[ID] -= 4
+        return state
+    return False
+
+def op_stone_pickaxe_for_cobble(state, ID):
+    if state.time[ID] >= 2 and state.stone_pickaxe[ID] >= 1:
+        state.cobble[ID] += 1
+        state.time[ID] -= 2
+        return state
+    return False
+
+def op_iron_pickaxe_for_cobble(state, ID):
+    if state.time[ID] >= 1 and state.iron_pickaxe[ID] >= 1:
+        state.cobble[ID] += 1
+        state.time[ID] -= 1
+        return state
+    return False
+
+# Advanced crafting (rail and cart)
+def op_craft_rail_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 6 and state.stick[ID] >= 1:
+        state.rail[ID] += 16
+        state.ingot[ID] -= 6
+        state.stick[ID] -= 1
+        state.time[ID] -= 1
+        return state
+    return False
+
+def op_craft_cart_at_bench(state, ID):
+    if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.ingot[ID] >= 5:
+        state.cart[ID] += 1
+        state.ingot[ID] -= 5
+        state.time[ID] -= 1
+        return state
+    return False
+
+# Register all operators
+pyhop.declare_operators(
+    op_punch_for_wood,
+    op_wooden_axe_for_wood,
+    op_stone_axe_for_wood,
+    op_iron_axe_for_wood,
+    op_craft_plank,
+    op_craft_stick,
+    op_craft_bench,
+    op_craft_wooden_axe_at_bench,
+    op_craft_wooden_pickaxe_at_bench,
+    op_craft_stone_axe_at_bench,
+    op_craft_stone_pickaxe_at_bench,
+    op_craft_furnace_at_bench,
+    op_craft_iron_axe_at_bench,
+    op_craft_iron_pickaxe_at_bench,
+    op_smelt_ore_in_furnace,
+    op_wooden_pickaxe_for_coal,
+    op_stone_pickaxe_for_coal,
+    op_iron_pickaxe_for_coal,
+    op_stone_pickaxe_for_ore,
+    op_iron_pickaxe_for_ore,
+    op_wooden_pickaxe_for_cobble,
+    op_stone_pickaxe_for_cobble,
+    op_iron_pickaxe_for_cobble,
+    op_craft_rail_at_bench,
+    op_craft_cart_at_bench
+)
 
 '''end operators'''
 
